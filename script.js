4842232ab1833179779a276871deafca6f35274c
Surjith16 <surjith326@gmail.com>
2024-09-07 17:12:28 +0530

    My portfolio

---
+++ script.js
@@ -0,0 +1,8 @@
+var typed= new Typed("#autotype",
+    {
+    strings : ["front-end developer","programmer","Designer","App-developer"],
+    typeSpeed:100,
+    backSpeed:100,
+    loop:true
+    })
+   
